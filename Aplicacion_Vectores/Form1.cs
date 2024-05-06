996f02fbdc06c6eaadabeeefc5c3a0099255f3ff
Zepo1 <lopezmar612@gmail.com>
2024-05-05 20:45:19 -0400

    first commit

---
+++ Aplicacion_Vectores/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicacion_Vectores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Vector V1 = new Vector();
+        Vector V2 = new Vector();
+        Vector V3 = new Vector();
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            V1.Dimensionar(int.Parse(textBox1.Text));
+        }
+
+        private void cargarParesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.CargarPares();
+        }
+
+        private void dESCARGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = V1.ToString();
+        }
+
+        private void cargarImparesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.CargarImpares();
+        }
+        private void cARGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox3.Text);  
+            int b = int.Parse(textBox4.Text);
+            V1.CargarRandom(a, b);
+        }
+        private void ordenarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.Ordenar();
+        }
+
+        private void insertarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int pos = int.Parse(textBox3.Text);
+            int valor= int.Parse(textBox4.Text);
+            V1.Insertar(pos, valor);
+        }
+
+        private void frecToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int digito = int.Parse(textBox3.Text);
+            textBox4.Text = string.Concat(V1.Frec(digito));
+        }
+
+        private void dESCARGARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            textBox7.Text = V2.ToString();
+        }
+
+        private void dESCARGARToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            textBox8.Text = V3.ToString();
+        }
+        private void cARGARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox3.Text);   
+            int b = int.Parse(textBox4.Text);
+            V2.CargarRandom(a, b);
+        }
+
+        private void cARGARToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox3.Text);
+            int b = int.Parse(textBox4.Text);
+            V3.CargarRandom(a, b);
+        }
+
+        private void dIMENSIONARToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            V2.Dimensionar(int.Parse(textBox1.Text));
+        }
+
+        private void dIMENSIONARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V3.Dimensionar(int.Parse(textBox1.Text));
+        }
+        private void verifOrdToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBox5.Text);
+            int b = int.Parse(textBox6.Text);
+            textBox6.Text = V1.VerifOrd(a, b).ToString();
+        }
+
+        private void fIbonacciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int ne = int.Parse(textBox5.Text);
+            V1.CargarFibonacciINV(ne);
+        }
+
+        private void dESCARGARToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = V1.ToString();
+        }
+
+        private void fibonacciDIVToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox6.Text = V1.FibonaDiv().ToString();
+        }
+
+        private void cargarfibonacciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int dim = int.Parse(textBox5.Text);
+            V1.fibonacciNormal(dim);
+        }
+
+        private void buscarElemMayorMultToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox7.Text= V1.BusElemMay2().ToString();
+           
+        }
+
+        private void multiplosNOMultiplosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int multiplo = int.Parse(textBox5.Text);
+            V1.Multiplos(V2, V3, multiplo);
+        }
+
+        private void invDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.invertirElem();
+        }
+
+        private void bucarMediaMult2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           textBox7.Text = V1.BusMediaDe2().ToString();
+        }
+
+        private void verificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool bandera = false;
+            if (V1.VeriIgual() == bandera)            
+                textBox7.Text = "FALSE";           
+            else           
+                textBox7.Text = "TRUE";
+            
+        }
+
+        private void cToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.ConjIgualdesen(V2);
+           textBox8.Text=V1.ToString();
+        }
+
+        private void conjuntoRepetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.ConjunIgulDesordenado(V2);
+            textBox8.Text = V1.ToString();
+        }
+
+        private void conjuntoDifereElemtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.BuscarDifeConjut(V2);
+            textBox8.Text = V1.ToString();
+        }
+
+        private void cARGARToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            String vector = textBox2.Text;
+            V1.Cargar(vector);
+        }
+
+        private void cARGARToolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            String vector = textBox7.Text;
+            V2.Cargar(vector);
+        }
+
+        private void verificarOrdeABToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool bandera = false;
+            if (V1.VerifOrdeAB(int.Parse(textBox5.Text),int.Parse(textBox6.Text)) == bandera)
+                textBox7.Text = "FALSE";
+            else
+                textBox7.Text = "TRUE";
+        }
+
+        private void eliminarNoPrimosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            V1.EliminarNoPrimos();
+            textBox7.Text = V1.ToString();
+        }
+    }
+}
---
+++ Aplicacion_Vectores/Vector.cs
@@ -0,0 +1,410 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Remoting.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacion_Vectores
+{
+    class Vector
+    {//Atributos
+        const int MaxE = 100;
+        int[] vector;
+        int dimension;
+        //Constructor
+        public Vector()
+        {
+            vector = new int[MaxE];
+            this.dimension = 0;
+
+        }
+        public int[] Getvector()
+        {
+             return vector;
+        }
+        public void Dimensionar(int dim)
+        {
+            this.dimension = dim;
+        }
+        public void adicionar(int x)
+        {
+            dimension++;
+            vector[dimension] = x;
+        }
+        public void Eliminar( int pos)
+        {
+          if (pos >= 1 && pos <= dimension)
+            {
+                for (int i = pos; i <= dimension - 1; i++)
+                {
+                    vector[i] = vector[i + 1]; 
+                }
+               dimension--; 
+            }
+        }
+        public void Poner(int pos, int x)
+        {
+            if (pos >= 1 && pos <= dimension)
+            {
+                vector[pos] = x;
+            }
+        }
+        
+        public void CargarPares()
+        { int x = 2;
+            for (int i = 1; i <= dimension; i++)
+            {
+                vector[i] = x;
+                x = x + 2;
+            }
+        }
+        public void CargarFibonacciINV(int ne)
+        {
+            this.dimension = ne;
+            int a = -1;
+            int b = 1;
+            int c;
+
+            for (int i = dimension; i >= 1; i--)
+            {
+                c = a + b;
+                vector[i] = c;
+                a = b; b = c;
+            }
+        }
+        public void fibonacciNormal(int dim)
+        {
+            this.dimension = dim;
+            int a = -1;
+            int b = 1;
+            int c;
+
+            for (int i = 1; i <= dim; i++)
+            {
+                c = a + b;
+                vector[i] = c;
+                a = b; b = c;
+            }
+        }
+        public void CargarImpares()
+        {
+            int x = 1;
+            for (int i = 1; i <= dimension; i++)
+            {
+                vector[i] = x;
+                x = x + 2;
+            }
+        }
+        public void CargarRandom(int a, int b)
+        {
+            Random r = new Random();
+            for (int i = 1; i <= dimension; i++)
+            {
+                vector[i] = r.Next(a, b + 1);
+            }
+
+        }
+        public void Cargar(String vector)
+        {
+            String numero = "";
+            for (int i = 0; i < vector.Length; i++)
+            {
+                if (vector[i] != ',')
+                {
+                    numero += vector[i].ToString();
+                }
+                else
+                {
+                    dimension++;
+                    this.vector[dimension] = int.Parse(numero);
+                    numero = "";
+                }
+            }
+
+            dimension++;
+            this.vector[dimension] = int.Parse(numero);
+        }
+
+        public int Dimension()
+        {
+            return dimension;
+        }
+
+        public void Ordenar() {
+            for (int i = 1; i <= dimension - 1; i++) {
+                for (int j = i + 1; j <= dimension; j++) {
+                    if (vector[j] < vector[i]) {
+                        int aux = vector[j];
+                        vector[j] = vector[i];
+                        vector[i] = aux;
+                    }
+                }
+            }
+        }
+        public void Insertar(int pos, int valor) {
+            if (pos >= 1 && pos <= dimension) {
+                dimension++;
+                int aux = dimension;
+                while (aux > pos)
+                {
+                    vector[aux] = vector[aux - 1];
+                    aux--;
+                }
+                vector[pos] = valor;
+            }
+        }
+        public int Frec(int digito)
+        {
+            int cant = 0;
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (vector[i] == digito) {
+                    cant++;
+                }
+            }
+            return cant;
+        }
+        public float FibonaDiv() {
+            int denominador = 2;
+            float acumulador = 0;
+            Boolean band = true;
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (band == true) {
+                    acumulador = acumulador + float.Parse((vector[i] / denominador).ToString());
+                    band = false;
+                } else
+                {
+                    acumulador = acumulador - float.Parse((vector[i] / denominador).ToString());
+                    band = true;
+                }
+                denominador = denominador + 2;
+            }
+            return acumulador;
+        }
+        public void Multiplos(Vector R1, Vector R2, int multiplo)
+        {
+
+            int n1 = 0; int n2 = 0;
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (vector[i] % multiplo == 0)
+                {//Es Multiplo
+                    n1++;
+                    R1.vector[n1] = vector[i];
+
+                }
+                else
+                {
+                    n2++;
+                    R2.vector[n2] = vector[i];
+
+                }
+            }
+            R1.dimension = n1;
+            R2.dimension = n2;
+        }
+        public Boolean VerifOrd(int a, int b)
+        {
+            Boolean r = true;
+            while (a < b)
+            {
+                if (vector[a] > vector[a + 1])
+                {
+                    r = false;
+                }
+                a++;
+            }
+            return r;
+        }
+
+        public String ToString()
+        {
+            String s = "[";
+            for (int i = 1; i <= dimension; i++)
+            {
+                s = s + vector[i] + "|";
+            }
+            // s= s.Remove(s.Length-1, 1);
+            s = s + "]";
+            return s;
+        }
+        public int BusElemMay2()
+        {
+            bool bandera = true; int aux = 0;
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (bandera == true)
+                    {
+                        aux = vector[i];
+                        bandera = false;
+                    }
+                    if (aux < vector[i])
+                    {
+                        aux = vector[i];
+                    }
+                }
+            }
+            return aux;
+        }
+        public int BusMediaDe2()
+        {
+            int aux = 0; int dim = 0;
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    aux = aux + vector[i];
+                    dim++;
+                }
+            }
+            return aux = aux / dim;
+        }
+        public void intercambiar(int posA, int posB)
+        {
+            int aux = vector[posA];
+            vector[posA] = vector[posB];
+            vector[posB] = aux;
+        }
+        public void invertirElem()
+        {
+            int aux = dimension / 2 ; int dim = dimension;
+            for(int i = 1; i <= aux ; i++)
+            {
+                intercambiar(i, dim);
+                dim--;
+            }
+
+        }
+        public bool VeriIgual()
+        {
+            bool bandera = false; int aux = 0;
+            for (int i = 1;i <= dimension; i++)
+            {
+                if (bandera == false)
+                {
+                    aux = vector[i];
+                    bandera = true;
+                }
+                if (vector[i] != aux)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void ConjIgualdesen(Vector vector2)
+        {
+            for (int i = 1; i <= vector2.dimension; i++)
+            {
+                this.adicionar(vector2.vector[i]);
+            }
+            
+            QuitarRepetOrdeDesen();
+        }
+        public void QuitarRepetOrdeDesen()
+        {
+            Ordenar();
+            int i = 1;
+            while (i < dimension)
+            {
+              if (vector[i] == vector[i + 1])
+                {
+                    Eliminar(i+1);                 
+                }
+                else
+                {
+                    i++;
+
+                }
+            }
+        }
+        public void ConjunIgulDesordenado(Vector V2)
+        {
+            for (int i = 1; i <= V2.dimension; i++)
+            {
+                this.adicionar(V2.vector[i]);
+            }
+            QuitarRepet();
+        }
+        public void QuitarRepet()
+        {
+           for(int i = 1; i <= dimension-1; i++)
+            {
+                for (int j = i+1; j <= dimension; j++)
+                {
+                    if ((vector[i] == vector[j]))
+                    {
+                        Eliminar(j);
+                    }
+                }
+            }
+                
+        }
+        public void BuscarDifeConjut(Vector Vec)
+        {
+            int i = 1; bool bandera=false;
+            while (i <= dimension)
+            {
+                for (int j = 1; j <= Vec.dimension; j++)
+                {
+                    if ((this.vector[i] == Vec.vector[j]))
+                    {
+                        Eliminar(i); 
+                        bandera = true;
+                        break;
+                    }
+                    else
+                    {
+                        bandera = false;
+                    }
+                }
+                if (bandera == false)
+                {
+                    i++;
+                }
+            }
+        }
+       public bool VerifOrdeAB(int A, int B )
+        {           
+            for (int i = A; i < B; i++)
+            {               
+                if (this.vector[i] > this.vector[i+1]) 
+                {
+                    return false;
+                }           
+            }
+            return true;
+        } 
+        public void EliminarNoPrimos()
+        {
+            for (int i = 1; i <= dimension; i++)
+            {
+                if (EsPrimo(i) == false)
+                {
+                    Eliminar(i);
+                }
+            }
+        }
+        public bool EsPrimo(int num)
+        {
+            if (num <= 1)
+                return false;
+            if (num <= 3)
+                return true;
+            if (num % 2 == 0 || num % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
